ef12c771e594bbfcfff08fa901a2721d508f7b3e
Guillermo-Padilla <guillermo.padillab@ug.edu.ec>
2023-06-30 10:06:54 -0500

    Taller_Proyecto

--- sap/settings.py
+++ sap/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'sap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hdsntlho',
+        'USER': 'hdsntlho',
+        'PASSWORD': '6Tek4LtGTxsJyztwmZimEAlI4LJa8Zdv',
+        'HOST': 'silly.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

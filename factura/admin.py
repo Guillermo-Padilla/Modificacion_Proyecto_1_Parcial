480d8a37ccf6bcbddb77876b6925c104dd0da173
Guillermo-Padilla <guillermo.padillab@ug.edu.ec>
2023-07-07 11:02:34 -0500

    Proyecto1P

--- factura/admin.py
+++ factura/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Factura, Cliente, Producto
+from .models import Factura, Cliente, Producto, Proveedor
 
 # Register your models here.
 admin.site.register(Factura)
 admin.site.register(Cliente)
 admin.site.register(Producto)
+admin.site.register(Proveedor)
